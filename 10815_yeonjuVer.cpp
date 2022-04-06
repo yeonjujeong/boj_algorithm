9fb7439c89785ba9f2f28833ed5e122f561b4343
yeonjujeong <jyj572788@naver.com>
2022-04-06 17:52:50 +0900

    add binary_10815

---
+++ 10815_yeonjuVer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr1[500001];
+
+int binary(int target, int start, int end){
+	if(start>end) return 0;
+	int mid = (start+end)/2;
+	
+	if(arr1[mid]==target) return 1;
+	else if(target>arr1[mid]) return binary(target,mid+1,end);
+	else if(target<arr1[mid]) return binary(target,start,mid-1);	
+}
+
+int main(){
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	int n; cin >> n;
+	for(int i=0;i<n;i++){
+		cin >> arr1[i];
+	}
+	sort(arr1,arr1+n);
+	int m; cin >> m;
+	int arr2[m];
+	for(int i=0;i<m;i++){
+		cin >> arr2[i];
+		cout << binary(arr2[i],0,n) << ' ';
+	}
+}
